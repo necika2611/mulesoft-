6b22908912a3c4ee6acd6ae7625c9472227687e8
nec2611 <99683489+nec2611@users.noreply.github.com>
2022-03-02 21:53:53 +0530

    Create SelectRecords.java

---
+++ SelectRecords.java
@@ -0,0 +1,48 @@
+import java.sql.DriverManager;  
+import java.sql.Connection;  
+import java.sql.ResultSet;  
+import java.sql.SQLException;  
+import java.sql.Statement;  
+   
+public class SelectRecords {  
+   
+    private Connection connect() {  
+       
+        String url = "jdbc:sqlite:C://sqlite/SSSIT.db";  
+        Connection conn = null;  
+        try {  
+            conn = DriverManager.getConnection(url);  
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        }  
+        return conn;  
+    }  
+   
+  
+    public void selectAll(){  
+        String sql = "SELECT * FROM movies";  
+          
+        try {  
+            Connection conn = this.connect();  
+            Statement stmt  = conn.createStatement();  
+            ResultSet rs    = stmt.executeQuery(sql);  
+              
+           
+            while (rs.next()) {  
+                System.out.println(rs.getInt("id") +  "\t" +   
+                                   rs.getString("name") + "\t" +  
+                                   rs.getDouble("capacity"));  
+            }  
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        }  
+    }  
+      
+     
+    
+    public static void main(String[] args) {  
+        SelectRecords app = new SelectRecords();  
+        app.selectAll();  
+    }  
+   
+}  
